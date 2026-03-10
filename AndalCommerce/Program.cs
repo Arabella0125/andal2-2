d03f47ad27c857ac2dd8e370a4d1cdf4b4d41f5d
Arabella0125 <andalarabellamae@gmail.com>
2026-03-10 11:52:50 +0800

    with validation and methods

--- AndalCommerce/Program.cs
+++ AndalCommerce/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AndalCommerce
 {
@@ -10,119 +11,243 @@ namespace AndalCommerce
         {
 
             Console.WriteLine("------ Address Selection ------");
-            Console.Write("Enter Full Name: ");
-            string name = Console.ReadLine();
+            string name = GetFullName();
+            string phone = GetPhone();
+            string fullAddress = GetFullAddress();
+            string postal = GetPostal();
 
-            if(string.IsNullOrWhiteSpace(name))
+            Console.WriteLine("\n------ Shipping Options ------");
+            string shippingName = GetShippingOption();
+            string paymentName = GetPaymentOption();
+
+
+
+            static string GetFullName()
             {
-                Console.WriteLine("Empty name. Please enter your name.");
-                return;
+                string name;
+
+                while (true)
+                {
+                    Console.Write("Enter Full Name: ");
+                    name = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(name))
+                    {
+                        Console.WriteLine("Name cannot be empty.");
+                        continue;
+                    }
+
+                    bool hasNumber = false;
+
+                    foreach (char c in name)
+                    {
+                        if (char.IsDigit(c))
+                        {
+                            hasNumber = true;
+                            break;
+                        }
+                    }
+
+                    if (hasNumber)
+                    {
+                        Console.WriteLine("Name cannot contain numbers.");
+                    }
+                    else
+                    {
+                        return name;
+                    }
+                }
             }
 
-            Console.Write("Enter Phone Number: ");
-            string phone = Console.ReadLine();
 
-            if (string.IsNullOrWhiteSpace(phone))
+            static string GetPhone()
             {
-                Console.WriteLine("Invalid phone number.");
-                return;
+                string phone;
+
+                while (true)
+                {
+                    Console.Write("Enter Phone Number: ");
+                    phone = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(phone))
+                    {
+                        Console.WriteLine("Phone number cannot be empty.");
+                        continue;
+                    }
+
+                    bool hasLetter = false;
+
+                    foreach (char c in phone)
+                    {
+                        if (!char.IsDigit(c))
+                        {
+                            hasLetter = true;
+                            break;
+                        }
+                    }
+
+                    if (hasLetter)
+                    {
+                        Console.WriteLine("Phone number must contain digits only.");
+                    }
+                    else if (phone.Length != 11)
+                    {
+                        Console.WriteLine("Phone number must be exactly 11 digits.");
+                    }
+                    else
+                    {
+                        return phone;
+                    }
+                }
             }
 
-            Console.Write("Enter Full Address: ");
-            string fullAddress = Console.ReadLine();
 
-            if (string.IsNullOrWhiteSpace(fullAddress)) 
+            static string GetFullAddress()
             {
-                Console.WriteLine("Empty address.");
-                return;
+                string fullAddress;
+
+                while (true)
+                {
+                    Console.Write("Enter Full Address: ");
+                    fullAddress = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(fullAddress))
+                    {
+                        Console.WriteLine("Address cannot be empty.");
+                    }
+                    else
+                    {
+                        return fullAddress;
+                    }
+                }
             }
 
-            Console.Write("Enter Postal Code: ");
-            string postal = Console.ReadLine();
-
-            if (string.IsNullOrWhiteSpace(postal))
+            static string GetPostal()
             {
-                Console.WriteLine("Invalid postal code.");
-                return;
+                string postal;
+
+                while (true)
+                {
+                    Console.Write("Enter Postal Code: ");
+                    postal = Console.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(postal))
+                    {
+                        Console.WriteLine("Postal code cannot be empty.");
+                        continue;
+                    }
+
+                    bool hasLetter = false;
+
+                    foreach (char c in postal)
+                    {
+                        if (!char.IsDigit(c))
+                        {
+                            hasLetter = true;
+                            break;
+                        }
+                    }
+
+                    if (hasLetter)
+                    {
+                        Console.WriteLine("Postal code must contain numbers only.");
+                    }
+                    else
+                    {
+                        return postal;
+                    }
+                }
             }
 
 
 
-            
-           
-                Console.WriteLine("\n------ Shipping Options ------");
-                Console.WriteLine("1. Standard Delivery (5-7 days)");
-                Console.WriteLine("2. Express Delivery (1-3 days)");
-                Console.WriteLine("3. Economy Delivery (7-14 days)");
-                Console.WriteLine("4. Same-Day Delivery");
-                Console.WriteLine("5. Store Pick-up (No shipping fee)");
-                Console.Write("Select Shipping Option (1-5): ");
-                string shippingOption = Console.ReadLine();
+            static string GetShippingOption()
+            {
+
+                string shippingOption;
                 string shippingName = "";
 
-                switch (shippingOption)
+                while (true)
                 {
-                    case "1":
-                        shippingName = "Standard Delivery (5-7 days)";
-                        break;
+                    Console.WriteLine("1. Standard Delivery (5-7 days)");
+                    Console.WriteLine("2. Express Delivery (1-3 days)");
+                    Console.WriteLine("3. Economy Delivery (7-14 days)");
+                    Console.WriteLine("4. Same-Day Delivery");
+                    Console.WriteLine("5. Store Pick-up (No shipping fee)");
+                    Console.Write("Select Shipping Option (1-5): ");
 
-                    case "2":
-                        shippingName = "Express Delivery (1-3 days)";
-                        break;
+                    shippingOption = Console.ReadLine();
 
-                    case "3":
-                        shippingName = "Economy Delivery (7-14 days)";
-                        break;
+                    switch (shippingOption)
+                    {
+                        case "1":
+                            shippingName = "Standard Delivery (5-7 days)";
+                            break;
+                        case "2":
+                            shippingName = "Express Delivery (1-3 days)";
+                            break;
+                        case "3":
+                            shippingName = "Economy Delivery (7-14 days)";
+                            break;
+                        case "4":
+                            shippingName = "Same-Day Delivery";
+                            break;
+                        case "5":
+                            shippingName = "Store Pick-up (No shipping fee)";
+                            break;
+                        default:
+                            Console.WriteLine("Invalid shipping option. Please choose between 1-5.");
+                            continue;
+                    }
 
-                    case "4":
-                        shippingName = "Same-Day Delivery";
-                        break;
+                    break;
+                }
 
-                    case "5":
-                        shippingName = "Store Pick-up (No shipping fee)";
-                        break;
+                return shippingName;
 
-                    default:
-                        Console.WriteLine("Invalid shipping option.");
-                        break;
-
-
-                
             }
 
 
-            Console.WriteLine("\n------ Payment Methods ------");
-            Console.WriteLine("1. Cash on Delivery");
-            Console.WriteLine("2. E-Wallet (Gcash, Maya, etc.");
-            Console.WriteLine("3. Payment Center");
-            Console.WriteLine("4. Online Banking (Debit/Credit)");
-            Console.Write("Select Payment Option (1-4): ");
-            string paymentOption = Console.ReadLine();
-            string paymentName = "";
-
-
-            switch (paymentOption)
+            static string GetPaymentOption()
             {
-                case "1":
-                    paymentName = "Cash on Delivery";
-                    break;
 
-                case "2":
-                    paymentName = "E-Wallet (Gcash, Maya, etc.)";
-                    break;
+                string paymentOption;
+                string paymentName = "";
 
-                case "3":
-                    paymentName = "Payment Center";
-                    break;
+                while (true)
+                {
+                    Console.WriteLine("\n------ Payment Methods ------");
+                    Console.WriteLine("1. Cash on Delivery");
+                    Console.WriteLine("2. E-Wallet (Gcash, Maya, etc.)");
+                    Console.WriteLine("3. Payment Center");
+                    Console.WriteLine("4. Online Banking (Debit/Credit)");
+                    Console.Write("Select Payment Option (1-4): ");
 
-                case "4":
-                    paymentName = "Online Banking (Debit/Credit)";
-                    break;
+                    paymentOption = Console.ReadLine();
 
-                default:
-                    Console.WriteLine("Invalid payment option.");
-                    break;
+                    switch (paymentOption)
+                    {
+                        case "1":
+                            paymentName = "Cash on Delivery";
+                            break;
+                        case "2":
+                            paymentName = "E-Wallet (Gcash, Maya, etc.)";
+                            break;
+                        case "3":
+                            paymentName = "Payment Center";
+                            break;
+                        case "4":
+                            paymentName = "Online Banking (Debit/Credit)";
+                            break;
+                        default:
+                            Console.WriteLine("Invalid payment option. Please choose between 1-4.");
+                            continue;
+                    }
 
+                    break;
+                }
+
+                return paymentName;
 
             }
 
